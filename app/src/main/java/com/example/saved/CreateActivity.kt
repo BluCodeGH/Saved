1d541eff66d0c150f875870efbad9fc92ce4f876
brennanrl <Brennan4441>
2019-09-14 13:06:42 -0400

    big commit

--- app/src/main/java/com/example/saved/CreateActivity.kt
+++ app/src/main/java/com/example/saved/CreateActivity.kt
@@ -9,56 +9,40 @@ import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
-
-
 private lateinit var auth: FirebaseAuth
-class MainActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         auth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_create)
     }
 
-    fun enter(view: View) {
+
+    fun accountCreate (view: View) {
         var email = findViewById<EditText>(R.id.email).text.toString()
         var password = findViewById<EditText>(R.id.password).text.toString()
-
-        val intent = Intent(this, MapsActivity::class.java)
+//        val intent = Intent(this, MapsActivity::class.java)
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-
                     // Sign in success, update UI with the signed-in user's information
 //                    Log.d(TAG, "createUserWithEmail:success")
                     val user = auth.currentUser
-                    /* updateUI(user) */
+                    val intentGood = Intent(this, MapsActivity::class.java)
+                    startActivity(intentGood)
                 } else {
                     // If sign in fails, display a message to the user.
 //                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
-                    /* updateUI(null) */
+                    val intentBad = Intent(this, MainActivity::class.java)
+                    startActivity(intentBad)
                 }
 
                 // ...
             }
 
-        startActivity(intent)
-
+        /* startActivity(intent) */
     }
-
-    fun createAccount (view: View) {
-        val randomIntent = Intent(this, CreateActivity::class.java)
-        startActivity(randomIntent)
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        /* updateUI(currentUser) */
-    }
-
-
 }
