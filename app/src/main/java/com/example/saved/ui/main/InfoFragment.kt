e4da79d8a78aa8d5db194310f39cc9450e866039
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2019-09-14 21:55:58 -0400

    super wip commit

--- app/src/main/java/com/example/saved/ui/main/InfoFragment.kt
+++ app/src/main/java/com/example/saved/ui/main/InfoFragment.kt
@@ -1,11 +1,17 @@
 package com.example.saved.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.saved.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,26 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_detail, container, false)
+        val id = arguments?.getString("id") as String
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference()
+        myRef.child("incidents").child(id).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val headline = dataSnapshot.child("headline").getValue() as String;
+                val body = dataSnapshot.child("body").getValue() as String;
+                view.findViewById<TextView>(R.id.title).setText(headline)
+                view.findViewById<TextView>(R.id.body).setText(body)
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("ME", "Failed to read value.", error.toException())
+            }
+        })
+        return view
     }
 }
